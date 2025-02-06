e109258145413c79bcccad1efe3000907b5030dc
Ian Zuber <ian.zuber.ext@bayer.com>
2025-02-06 17:04:01 -0500

    Small auth unit tests added

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  const mockAuthService = {};
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService, JwtService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -14,5 +19,17 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should have a login method', () => {
+    expect(controller.login).toBeDefined();
+  });
+  it('should have a login method', () => {
+    expect(controller.login).toBeDefined();
+  });
+  it('should have a register method', () => {
+    expect(controller.register).toBeDefined();
+  });
+  it('should have a profile method', () => {
+    expect(controller.getProfile).toBeDefined();
   });
 });
